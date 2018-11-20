509f17bfb93704f10425c721431e4282d5490daf
Martin Ewald <ewa@oc-ag.ch>
2018-11-20 08:16:25 +0100

    added components placeholder and field-user-renderer from spfx-controls-react

--- index.ts
+++ index.ts
@@ -1,5 +1,7 @@
-export {ColorPicker} from './color-picker';
-
+export { ColorPicker } from './src/color-picker';
+export { Persona } from './src/persona';
+export { Placeholder } from './src/placeholder';
+export { FieldUserRenderer } from './src/field-user-renderer';
 
 // npm run build -> compiles typescript
 // npm publish -> publishes the npm package
